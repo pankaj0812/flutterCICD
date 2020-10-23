4642a5de49e5e1d2a41579067e2506b3bd9e20d2
pankaj0812 <singhpankajkumar65@gmail.com>
2020-10-23 18:49:00 +0530

    Added new app bar title

--- lib/main.dart
+++ lib/main.dart
@@ -69,10 +69,16 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          // title: Text(widget.title),
+          title: Row(
+        children: <Widget>[
+          Text('Github'),
+          Icon(Icons.favorite_border),
+          Text('Flutter')
+        ],
+      )),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
